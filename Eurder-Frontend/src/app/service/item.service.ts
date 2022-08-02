dce06720e87064c3bd2c2fc850ed58b256255410
DracKunn <brecht.vanrie@gmail.com>
2022-08-02 10:17:18 +0200

    added pipe to sort items alphabetically

--- Eurder-Frontend/src/app/service/item.service.ts
+++ Eurder-Frontend/src/app/service/item.service.ts
@@ -1,7 +1,8 @@
 import {Injectable} from '@angular/core';
 import {HttpClient} from "@angular/common/http";
 import {environment} from "../../environments/environment";
-import {Observable} from "rxjs";
+import {map, Observable} from "rxjs";
+import {Item} from "../../model/Item";
 
 @Injectable({
   providedIn: 'root'
@@ -15,8 +16,12 @@ export class ItemService {
     this.backendItems = `${environment.backendUrl}/items`
   }
 
-  getItems(): Observable<any> {
-    return this.http.get(this.backendItems)
+  getItems(): Observable<Item[]> {
+    return this.http.get<Item[]>(this.backendItems).pipe(
+      map((item)=>item.sort(
+        (item1,item2)=>{return item1.name.localeCompare(item2.name)}
+      ))
+    )
   }
 }
 
